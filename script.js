3d10afdf9d94e1c950d6e71666574b6ff0ab81cb
gidra <gidraiver@gmail.com>
2022-11-30 12:50:50 +0300

    pepedance

--- script.js
+++ script.js
@@ -1,3 +1,16 @@
+const observer = new IntersectionObserver(entries => {
+    // Loop over the entries
+    entries.forEach(entry => {
+      // If the element is visible
+      if (entry.isIntersecting) {
+        // Add the animation class
+        entry.target.classList.add('pepe-animation');
+      }
+    });
+  });
+  
+  observer.observe(document.querySelector('.pepedance'));
+
 function myFunction() {
     var popup = document.getElementById("myPopup");
     popup.classList.toggle("show");
